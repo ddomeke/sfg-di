177344174d6949b440ea4920985dd2df0b9c0ff6
ddomeke <durandomeke@gmail.com>
2024-04-22 12:00:50 +0300

    Primary Annotation usage

--- src/main/java/guru/springframework/sfgdi/controller/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -1,14 +1,19 @@
 package guru.springframework.sfgdi.controller;
 
+import guru.springframework.sfgdi.services.GreetingServices;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private  final GreetingServices greetingServices;
+
+    public MyController(GreetingServices greetingServices) {
+        this.greetingServices = greetingServices;
+    }
+
     public String sayHello(){
 
-        System.out.println("Hello World");
-
-        return "Hi Folks";
+        return greetingServices.sayGreeting();
     }
 }
