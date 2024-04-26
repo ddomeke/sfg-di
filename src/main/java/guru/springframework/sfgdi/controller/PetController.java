e2eb81f132b19397844c2957829f4cca248fd0af
ddomeke <durandomeke@gmail.com>
2024-04-26 12:53:29 +0300

    life_cycle_of_bean

--- src/main/java/guru/springframework/sfgdi/controller/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controller/PetController.java
@@ -4,9 +4,7 @@ import guru.springframework.sfgdi.services.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 12/28/19.
- */
+
 @Controller
 public class PetController {
 
